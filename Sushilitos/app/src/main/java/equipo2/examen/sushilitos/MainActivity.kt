d0e270c657366ffec52aa928f050d18cdbe83130
Fernando <ferschool99@gmail.com>
2020-03-13 19:15:20 -0700

    Agregue la parte del inicio

--- Sushilitos/app/src/main/java/equipo2/examen/sushilitos/MainActivity.kt
+++ Sushilitos/app/src/main/java/equipo2/examen/sushilitos/MainActivity.kt
@@ -3,6 +3,8 @@ package equipo2.examen.sushilitos
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,10 +12,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        menu.setOnClickListener{
+            var intent = Intent(this, Categorias::class.java)
+            intent.putExtra("tipo", "Cold drinks")
+            startActivity(intent)
+        }
+
         //La pondre directo porque no tengo boton en la vista principal
-        var intent = Intent(this, Categorias::class.java)
-        intent.putExtra("tipo", "Cold drinks")
-        startActivity(intent)
+
 
     }
 }
